9da1f2366521170b361f7c6b8b607e5b0506f68e
Delyan Kompanski <HSHristov18@codingburgas.bg>
2021-07-10 08:48:27 +0300

    added more markers on the map

--- scripts/map.js
+++ scripts/map.js
@@ -1,15 +1,36 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
+  // The location of Burgas
   const burgas = { lat: 42.513, lng: 27.466 };
-  // The map, centered at Uluru
+  const sofia = { lat: 42.697, lng: 23.321 };
+  const plovdiv = { lat: 42.144, lng: 24.750 };
+  const varna = { lat: 43.214, lng: 27.914 };
+  // The map, centered at Burgas
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 7,
     center: burgas,
   });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
+  // The marker, positioned at Burgas
+   marker = new google.maps.Marker({
     position: burgas,
+    map: map,
+  });
+
+  // The marker, positioned at Burgas
+  marker = new google.maps.Marker({
+    position: sofia,
+    map: map,
+  });
+
+  // The marker, positioned at Burgas
+  marker = new google.maps.Marker({
+    position: varna,
+    map: map,
+  });
+
+  // The marker, positioned at Burgas
+  marker = new google.maps.Marker({
+    position: plovdiv,
     map: map,
   });
 
